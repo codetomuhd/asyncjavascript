957847bdfcfe3c293cc7948dce28fa58534c32bf
Muhammad <bukarmuhd@gmail.com>
2021-03-08 13:32:06 +0100

    Promise Api written successfully!

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,52 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState);
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('could not fetch data', undefined);
-        }
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', () => {
+            // console.log(request, request.readyState);
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting data');
+            }
+        });
+
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+        request.open('GET', resource);
+
+        request.send();
     });
 
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-    request.open('GET', resource);
-
-    request.send();
 };
 
-getTodos('todos/muhd.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/musa.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/yusuf.json', (err, data) => {
-            console.log(data);
-        });
-    });
+getTodos('todos/muhd.json').then((data) => {
+    console.log('Promise resolved: ', data);
+}).catch(err => {
+    console.log('Promise rejected: ', err);
 });
+
+//promise example
+
+// const getSomething = () => {
+
+//     return new Promise((resolve, reject) => {
+//         // fetch some data
+//         resolve('Some data');
+//         // reject('some error');
+//     });
+
+// };
+
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
