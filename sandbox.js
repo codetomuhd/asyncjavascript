f7160af9d65479076e40899aba247748a1c89897
Muhammad <bukarmuhd@gmail.com>
2021-03-08 12:10:33 +0100

    callback hell demo code completed

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
@@ -12,21 +12,17 @@ const getTodos = (callback) => {
     });
 
     // request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-    request.open('GET', 'todos.json');
+    request.open('GET', resource);
 
     request.send();
 };
 
-console.log(1);
-console.log(2);
-getTodos((err, data) => {
-    console.log('callback fired!');
-    if (err) {
-        console.log(err);
-    } else {
+getTodos('todos/muhd.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/musa.json', (err, data) => {
         console.log(data);
-    }
+        getTodos('todos/yusuf.json', (err, data) => {
+            console.log(data);
+        });
+    });
 });
-
-console.log(3);
-console.log(4);
