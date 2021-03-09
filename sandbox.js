aafea04fd93810690a46e07df263ec2d2dad4503
Muhammad <bukarmuhd@gmail.com>
2021-03-09 03:45:36 +0100

    The Async and Await  Api code completed successfully!

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,25 @@
+
 // The Fetch Api Example
-fetch('todos/muhd.json').then(response => {
-    console.log('Resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch(error => {
-    console.log('Rejected', error);
-});
+const getTodos = async () => {
+    const response = await fetch('todos/muhd.json');
+
+    if (response.status !== 200) {
+        throw new Error('could not fetch data!');
+    }
+
+    const data = response.json();
+    return data;
+};
+
+getTodos()
+    .then(data => console.log('Resolved',data))
+    .catch(err => console.log('Rejected',err));
+
+// fetch('todos/muhd.json').then(response => {
+//     console.log('Resolved', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.log('Rejected', error);
+// });
